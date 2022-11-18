67091aaab0448a62c5018c9a172bb37e67d954d6
Andrii <68606253+Envil2001@users.noreply.github.com>
2022-11-17 17:52:33 -1200

    update tags validation 2

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -21,7 +21,7 @@ router.delete('/posts/:id', checkAuth, controllers.remove);
 router.patch('/posts/:id', checkAuth, [
     body('title', 'Введите заголовок статьи').isLength({min: 3}).isString(),
     body('text', 'Введите текст статьи').isLength({ min: 3}).isString(),
-    body('tags', 'Неверный формат тэгов (укажите массив)').optional().isString(),
+    // body('tags', 'Неверный формат тэгов (укажите массив)').optional().isString(),
     body('imageUrl', 'Неверная ссылка на изображение').optional().isString()
 ], handleValidationErrors, controllers.update);
 module.exports = router;
